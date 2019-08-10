78a1c62192947ac14cc4b43a98e3db5333ecd7bb
spacedome <julien@spacedome.tv>
2019-08-10 16:16:07 -0400

    Initial (kinda working) code and test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using FEASTSolver
 using Test
+using LinearAlgebra: diagm
 
 @testset "FEASTSolver.jl" begin
-    # Write your own tests here.
+
+    A = diagm(0 => 1:25)
+    # e, v, res = feast!(A, rand(25, 5), 4, 10, c=2.0, r=2.0)
+    e, v, res = feast!(A, rand(ComplexF64, 25, 5), 8, 10, c=2.0, r=2.0)
+    @test sort(real.(e)) ≈ 1:5
+    @test maximum(res) < 1e-8
+
 end
