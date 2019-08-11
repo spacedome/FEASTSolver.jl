378ff1e429e7765932747e3e600f6038402364a3
spacedome <julien@spacedome.tv>
2019-08-10 20:11:17 -0400

    Generalized FEAST sorta kinda working, dual subspace broken

---
+++ src/beyn.jl
@@ -0,0 +1,35 @@
+using LinearAlgebra: ldiv!, lu!, mul!, eigen!, norm, Diagonal
+
+export beyn
+
+function beyn(T::Function, A::AbstractMatrix, X::AbstractMatrix, nodes::Integer;
+                c=complex(0.0,0.0), r=1.0)
+    N, m₀ = size(X)
+    if size(A, 1) != size(A, 2)
+        error("Incorrect dimensions of A, must be square")
+    elseif size(A,1) != N
+        error("Incorrect dimensions of X₀, must match A")
+    end
+    res = zeros(m₀)
+    temp = zeros(ComplexF64, N, m₀)
+
+    Q₀, Q₁ = zeros(ComplexF64, N, m₀), zeros(ComplexF64, N, m₀)
+    ZmA = similar(A, ComplexF64)
+    θ = LinRange(π/nodes, 2*π-π/nodes, nodes)
+    for i=1:nodes
+        ZmA .= T(r*exp(θ[i]*im)+c)
+        ldiv!(temp, lu!(ZmA), X)
+        Q₀ .+= temp .* exp(θ[i]*im)./nodes
+        Q₁ .+= (r*exp(θ[i]*im)+c) .* temp .* exp(θ[i]*im)./nodes
+    end
+    S = svd!(Q₀)
+    Aq = S.U' * Q₁ * S.V * inv(Diagonal(S.S))
+    Λ, Xq = eigen!(Aq)
+    X = S.U * Xq
+    for i=1:m₀
+            # res[i] = norm(T(Λ[i])*X[:,i])/norm(T(c)*X[:,i])
+        res[i] = norm(T(Λ[i])*X[:,i])
+    end
+    p = sortperm(res)
+    Λ[p], X[:, p], res[p]
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,31 @@
 using FEASTSolver
 using Test
-using LinearAlgebra: diagm
+using LinearAlgebra: diagm, Diagonal
+
+function approx_in(x::Number, A)
+    reduce(|, x .≈ A)
+end
+
+function approx_in(X::AbstractArray, A)
+    reduce(|, [approx_in(x, A) for x in X])
+end
 
 @testset "FEASTSolver.jl" begin
 
     A = diagm(0 => 1:25)
     # e, v, res = feast!(A, rand(25, 5), 4, 10, c=2.0, r=2.0)
-    e, v, res = feast!(A, rand(ComplexF64, 25, 5), 8, 10, c=2.0, r=2.0)
-    @test sort(real.(e)) ≈ 1:5
-    @test maximum(res) < 1e-8
+    e, v, res = feast!(rand(ComplexF64, 25, 5), A, nodes=8, iter=10, c=1.5, r=2.0)
+    @test approx_in(1:3, real.(e))
+    @test maximum(sort(res)[1:3]) < 1e-12
+    e, v, res = gen_feast!(rand(ComplexF64, 25, 5), A, Diagonal(ones(25)), nodes=8, iter=100, c=1.5, r=2)
+    println(real.(e))
+    println(res)
+    # @test approx_in(1:3, real.(e))
+    @test maximum(sort(res)) < 1e-12
+    # e, v, res = dual_gen_feast!(rand(ComplexF64, 25, 5), A, Diagonal(ones(25)), nodes=16, iter=100, c=2.0, r=2.5)
+    # println(real.(e))
+    # println(res)
+    # # @test approx_in(1:4, real.(e))
+    # @test maximum(sort(res)) < 1e-12
 
 end
