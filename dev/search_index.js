c9f5669662238c147d8de9b81e2ad23e765beace
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-11 01:04:29 +0000

    build based on d1fdec9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FEASTSolver.jl-1","page":"Home","title":"FEASTSolver.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [FEASTSolver]","category":"page"}]
+}
